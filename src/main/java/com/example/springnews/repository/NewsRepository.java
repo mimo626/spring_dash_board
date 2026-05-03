58541c618959dc9014155ff9adfe26aeaaaa235e
kangminju <yessuk5499@gmail.com>
2026-05-03 18:21:32 +0900

    NewsRepository 메서드 작성

--- src/main/java/com/example/springnews/repository/NewsRepository.java
+++ src/main/java/com/example/springnews/repository/NewsRepository.java
@@ -1,14 +1,36 @@
 package com.example.springnews.repository;
 
 import com.example.springnews.model.entity.News;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface NewsRepository extends JpaRepository<News, Integer> {
-//    뉴스글 작성
-//    뉴스글 전체 읽기
+//    뉴스글 작성 save(news)
+
+//    뉴스글 전체 읽기 findAll()
+
 //    id 로 글 한 개 읽기
+    public News findNewsById(int id);
+
 //    뉴스글 삭제
+    public boolean deleteNewsById(int id);
+
 //    뉴스글 변경
+    public boolean updateNewsById(News news);
+
 //    뉴스 글 내용에서 검색
+    @Query("SELECT n FROM News n WHERE n.content LIKE :content")
+    public List<News> searchNews(String content);
+
 //    글 한 개를 읽은 경우에는 cnt 변경
+    @Modifying
+    @Transactional
+    @Query("UPDATE News n SET n.title = :title + 1 WHERE n.id = :id")
+    int updateNewsCnt(@Param("id") int id, @Param("cnt") int cnt);
+
 }
