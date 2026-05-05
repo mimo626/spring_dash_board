a5f399bdc742941f8878a55403337a57f19414e7
kangminju <yessuk5499@gmail.com>
2026-05-05 23:50:16 +0900

    NewsRepository 삭제 함수 수정

--- src/main/java/com/example/springnews/repository/NewsRepository.java
+++ src/main/java/com/example/springnews/repository/NewsRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
@@ -17,20 +18,22 @@ public interface NewsRepository extends JpaRepository<News, Integer> {
 //    id 로 글 한 개 읽기
     public News findNewsById(int id);
 
-//    뉴스글 삭제
-    public boolean deleteNewsById(int id);
+    public List<News> findNewsByWriter(String writer);
 
-//    뉴스글 변경
-    public boolean updateNewsById(News news);
+//    뉴스글 삭제
+    @Transactional
+    public int deleteNewsById(int id);
+
+//    뉴스글 변경 save(news)
 
 //    뉴스 글 내용에서 검색
-    @Query("SELECT n FROM News n WHERE n.content LIKE :content")
+    @Query("SELECT n FROM News n WHERE n.content LIKE %:content%")
     public List<News> searchNews(String content);
 
 //    글 한 개를 읽은 경우에는 cnt 변경
     @Modifying
     @Transactional
-    @Query("UPDATE News n SET n.title = :title + 1 WHERE n.id = :id")
+    @Query("UPDATE News n SET n.cnt = :cnt + 1 WHERE n.id = :id")
     int updateNewsCnt(@Param("id") int id, @Param("cnt") int cnt);
 
 }
